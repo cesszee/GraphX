6c6e32891d099036a325e63749a91304368cf0ec
cesszee <francesiehsiamvilla@gmail.com>
2026-08-10 18:38:13 +0800

    initial program Graphx working

--- src/main.asm
+++ src/main.asm
@@ -2,11 +2,17 @@
 .model flat, stdcall
 option casemap:none
 
+MessageBoxA PROTO STDCALL :DWORD, :DWORD, :DWORD, :DWORD
 ExitProcess PROTO STDCALL :DWORD
+
+.data
+msgText  db "GraphX is running!",0
+msgTitle db "GraphX",0
 
 .code
 
 start:
+    invoke MessageBoxA, 0, ADDR msgText, ADDR msgTitle, 0
     invoke ExitProcess, 0
 
 END start
